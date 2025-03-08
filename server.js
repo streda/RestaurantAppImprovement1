e1391e71f61e7b3bede93184f7cdc66589d35c0e
streda <samuelreda2018@gmail.com>
2025-03-08 09:36:14 -0800

    Fix authentication, session management, and JWT handling

--- server.js
+++ server.js
@@ -384,9 +384,8 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-const PORT = process.env.PORT || 5005;
-app.listen(PORT, () => {
-});
+const PORT = process.env.PORT || 5005; // Heroku dynamically assigns a PORT
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 // app.listen(PORT, async () => {
 //   // Open the default browser
 //   await open(`http://localhost:${PORT}`);
